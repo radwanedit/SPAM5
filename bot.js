46b8cd05c3ea7b0071ddbcfded628190a7b53a0e
radwanedit <46229334+radwanedit@users.noreply.github.com>
2019-04-12 19:02:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,544 +1,131 @@
-const { Client, RichEmbed } = require('discord.js');
-
-// Create an instance of a Discord client
-const client = new Client();
-const client2 = new Client();
-const client3 = new Client();
-const client4 = new Client();
-const client5 = new Client();
-const client6 = new Client();
-const client7 = new Client();
-const client8 = new Client();
-const client9 = new Client();
-const client10 = new Client();
-const client11 = new Client();
-const client12 = new Client();
-const client13 = new Client();
-const client14 = new Client();
-const client15 = new Client();
-const client16 = new Client();
-const client17 = new Client();
-
-
-
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const client2 = new Discord.Client();
+const myid = [''];
+const prefix = ['-'];
 
 client.on('ready', () => {
-  console.log('I am ready!');
+   console.log(`----------------`);
+   console.log(`Credit Farmm - Script By : Kahrbaa `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
+});
+
+client2.on('ready', () => {
+   console.log(`----------------`);
+   console.log(`2 Account Online ✩' `);
+   console.log(`----------------`);
 });
 
 
 
-client.on('message',function(message) {
-	let prefix = "!";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
+client.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send("#rep "+"<@" + myid + ">")
+    }
+});
+
+client.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 20000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
+});
+
+client2.on('message', message => {
+    if(message.content === prefix+'راتب'){
+        message.channel.send('#daily')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === prefix+'مبلغ'){
+        message.channel.send('#credits')
+    }
+});
+
+client2.on('message', message => {
+    if(message.content === prefix+'ريب'){
+        message.channel.send('#rep <@286088294234718209>')
+    }
+});
+
+client2.on('message', message => {
+if (message.content === prefix+'spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 20000; x++) {
+        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+          .then(m => {
+            count++;
+          })
+          
+        }
+      }
+});
+
+client2.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+
+
+if (command == "تحدث") {
+let rank = message.guild.member(message.author).roles.find('name', 'Role.Kahrbaa');
+if (!rank) return message.reply(' ')
+  message.channel.send(args.join("  "))
+    message.delete();
+  }
 });
 
 
-     client.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-		 
-		 
-	client2.on('message',function(message) {
-	let prefix = "!!";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client2.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });	 
-		 		 
-		 
-		 
-		 
-	client3.on('message',function(message) {
-	let prefix = "!!!";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client3.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });	 
-		 
-		 
-client4.on('message',function(message) {
-	let prefix = "!!!!";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client4.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-		 
-		 
-		 
-client5.on('message',function(message) {
-	let prefix = "!!!!!";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client5.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-		 
-client6.on('message',function(message) {
-	let prefix = "+";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client6.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-		 
-client7.on('message',function(message) {
-	let prefix = "++";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client7.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-
-	 		 
-client8.on('message',function(message) {
-	let prefix = "+++";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client8.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-	 
-	 
-	 
-	 		 
-client9.on('message',function(message) {
-	let prefix = "++++";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client9.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-	 
-	 		 
-client10.on('message',function(message) {
-	let prefix = "+++++";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client10.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(` الو ظوبرى فى طيزك ههه
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-	 
-	 
-
-
-client11.on('message',function(message) {
-	let prefix = "=";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client11.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-client12.on('message',function(message) {
-	let prefix = "=";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client12.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-	 
-
-client12.on('message',function(message) {
-	let prefix = "=";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client12.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-		 
-		 
-client13.on('message',function(message) {
-	let prefix = "==";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client13.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-		 
-	
-
-		 
-client14.on('message',function(message) {
-	let prefix = "==";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client14.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-		 
-
-
-
-
-
-
-client15.on('message',function(message) {
-	let prefix = "=";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client15.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-client16.on('message',function(message) {
-	let prefix = "=";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client16.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-
-
-client17.on('message',function(message) {
-	let prefix = "=";
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith(prefix + "say")) {
-if(!args) return;
-message.channel.send(`${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
-}
-});
-
-
-     client17.on('message', message => {
-         if (message.content === 'كسمكم') {
-               let count = 0;
-               let ecount = 0;
-               for(let x = 0; x < 10000; x++) {
-                 message.channel.send(`Galal Hey Farming Prro hhhahahahahahahahhaahahha
-${x}`)
-                   .then(m => {
-                     count++;
-                   })
-
-                 }
-               }
-         });
-	 
-
-
-
-
-
-
-client.login("TOKEN")
-client2.login("TOKEN1")
-client3.login("TOKEN2")
-client4.login("TOKEN3")
-client5.login("TOKEN4")
-client6.login("TOKEN5")
-client7.login("TOKEN6")
-client8.login("TOKEN7")
-client9.login("TOKEN8")
-client10.login("TOKEN9")
-client11.login("TOKEN10")
-client12.login("TOKEN11")
-client13.login("TOKEN12")
-client14.login("TOKEN13")
-client15.login("TOKEN14")
-client16.login("TOKEN15")
-client17.login("TOKEN16")
-
-
-
-
-
+client.login(process.env.TOKEN);
+client2.login(process.env.TOKEN2);
